f94380e392ca65a5cf17cdb53a9c6e95520bb389
Simona Stanciu <simona.stanciu92@gmail.com>
2021-04-02 10:44:48 +0300

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -1,11 +1,21 @@
-const extendedMenu = document.getElementById('extendedMenu');
-const mobileMenu = document.getElementById('mobileMenu');
-const closeButton = document.getElementById('closeButton');
+// const extendedMenu = document.getElementById('extendedMenu');
+// const mobileMenu = document.getElementById('mobileMenu');
+// const closeButton = document.getElementById('closeButton');
 
-mobileMenu.addEventListener ('click', function() {
-    extendedMenu.classList.remove('hidden');
-});
+// mobileMenu.addEventListener ('click', function() {
+//     extendedMenu.classList.remove('hidden');
+// });
 
-closeButton.addEventListener ('click', function() {
-    extendedMenu.classList.add('hidden');
-});
+// closeButton.addEventListener ('click', function() {
+//     extendedMenu.classList.add('hidden');
+// });
+
+$(function () {
+    $('#mobileMenu').click(function () {
+        $('#extendedMenu').removeClass('hidden');   
+    });
+
+    $('#closeButton').click(function () {
+        $('#extendedMenu').addClass('hidden');
+    });
+})
